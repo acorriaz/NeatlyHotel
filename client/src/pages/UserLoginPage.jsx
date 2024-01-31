eef960ea7528742b054c5878e2c3515c4d62838d
datfoi <jengjiening@gmail.com>
2024-01-31 15:17:04 +0700

    feat: add login form component, tailwind color&text set, add register page

---
+++ client/src/pages/UserLoginPage.jsx
@@ -0,0 +1,12 @@
+import { UserLoginForm } from "../components/LogInForm";
+
+function UserLoginPage() {
+  return (
+    <>
+      {/* navbar area */}
+      <UserLoginForm />
+    </>
+  );
+}
+
+export default UserLoginPage;
