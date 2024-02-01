05a9146479d7d53bad4f549fc10a3fd0648aa3ad
acorriaz <mikey.swx@gmail.com>
2024-02-01 16:35:05 +0700

    feat: add components for landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLoginPage from "./pages/UserLoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/hotel/user-login" element={<UserLoginPage />} />
           <Route path="/hotel/user-register" element={<RegisterPage />} />
+          <Route path="/hotel" element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </>
