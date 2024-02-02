aaa17e35b6484f37a66457334b698b92258ea984
acorriaz <mikey.swx@gmail.com>
2024-02-02 12:54:45 +0700

    feat: add user reviews components

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import AboutHotel from "../components/landing-page/AboutHotel";
 import ServiceAndFacilities from "../components/landing-page/ServiceAndFacilities";
 import RoomsAndSuits from "../components/landing-page/RoomsAndSuits";
 import Footer from "../components/Footer";
+import Review from "../components/landing-page/Review";
 
 export default function LandingPage() {
   return (
@@ -11,6 +12,7 @@ export default function LandingPage() {
       <AboutHotel />
       <ServiceAndFacilities />
       <RoomsAndSuits />
+      <Review />
       <Footer />
     </div>
   );
