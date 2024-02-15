1ff90d478dd3262188e8b4ac81d4c9ac395d4139
acorriaz <mikey.swx@gmail.com>
2024-02-15 14:18:15 +0700

    feat: modified search page to sent room data to payment page

--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -1,10 +1,11 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import SearchBar from "./utils/SearchBar.jsx";
 
 function SearchResult() {
   const location = useLocation();
+  const navigate = useNavigate();
   const receivedData = location.state;
   const [guestCount, setGuestCount] = useState("");
   const [rooms, setRooms] = useState([]);
