624962580303147b905a3a7d2139430b4df0d773
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2024-02-16 20:02:16 +0700

    fix: remove duplicate import on search result

--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -1,5 +1,4 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import SearchBar from "./utils/SearchBar.jsx";
