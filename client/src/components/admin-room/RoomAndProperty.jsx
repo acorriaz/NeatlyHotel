b7241a9978f751ee49d49e52dc96573a5d565f2d
acorriaz <mikey.swx@gmail.com>
2024-03-07 12:32:24 +0700

    fix: uncommited change

--- client/src/components/admin-room/RoomAndProperty.jsx
+++ client/src/components/admin-room/RoomAndProperty.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import searchVector from "../../assets/admin/searchVector.svg";
 import SideBar from "../admin/SideBar";
 
